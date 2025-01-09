ecc21b08e5abe6e995766429b8efeca3f8d848a3
unknown <dilafruztoshmatova209@gmail.com>
2025-01-09 19:28:33 +0500

    uyga vazifa

---
+++ app.js
@@ -0,0 +1,147 @@
+// 1-masala
+
+// let k = 7
+// let n = 5
+
+// for(let i =1; i<=n; i++){
+//     console.log(k)
+// }
+
+
+// 2-masala
+
+// let narx = 5790;
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${i / 10} kg = ${+((i / 10) * narx).toFixed(2)} so'm`)
+
+// }
+
+// 3-masala
+
+// let n = 5; 
+// let S = 0;
+
+// for (let i = n; i <= 2 * n; i++) {
+//     S += i ** 2; 
+// }
+
+// console.log(S)
+
+// 4-masala
+
+// let N = 5; 
+// let a = 0;
+
+// for (let i = 1; i <= N; i++) {
+//     a += i ** (N - i + 1);
+// }
+
+// console.log( sum);
+
+
+// 5-masala
+
+    // let A = 1;
+    // let B = 10;
+    
+    // for (let i = A; i <= B; i++) {
+    //     let repate = i.toString().repeat(i); 
+    //     let number = Number(repate);
+    //     console.log(number);       
+    // }
+       
+
+// 6-masala
+
+// let son = 10;
+
+// for(let i = 1; i <= son; i++){
+//   if(10 % i == 0){
+//     console.log(i)
+//   }
+// }
+
+
+// 7-masala
+
+// let N = 20
+// let K = 7
+
+// let qoldiq = N
+// let butun = 0
+
+// while (qoldiq >= K) {
+//     qoldiq -= K
+//     butun += 1
+// }
+
+// console.log(`butun ${butun} qismi, qoldiq ${qoldiq} qismi`)
+
+// 8-masala
+
+// let daraja = 7;
+// let i = 2;
+
+// while (i <= 2) {
+//     let result = Math.pow(2, daraja);
+//     console.log( result);
+//     i++;    
+// }
+
+
+// 9-masala
+
+// let masala = (a, b) => {
+//     const numbers = [];
+//     for (let i = a; i <= b; i++) {
+//         numbers.push(i);
+//     }
+//     console.log(numbers)
+//     console.log(numbers.length);
+// }
+
+// masala(5, 20)
+
+// 10-masala
+
+// function masala(a, b) {
+
+//     const numbers = [];
+//     for (let i = b - 1; i > a; i--) {
+//         numbers.push(i);
+//     }
+    
+//     console.log( numbers);
+//     console.log( numbers.length);
+// }
+
+// masala(5, 20);
+
+
+// 11-masala
+
+// let narx = 5790
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${i} kg = ${+((i) * narx).toFixed(2)} so'm`)
+// }
+
+// 12-masala
+
+// let narx = 5790
+
+
+// for(let i = 1; i <= 10; i++ ){
+//     console.log(`${i / 10} kg = ${+((i / 10) * narx).toFixed(2)} so'm`)
+// }
+
+//chala shun qilolmadim
+
+// 13-masala 
+
+// let a = 5
+// let b = 20
+// for (let i = a; i <= b; i++) {
+//     console.log(i);
+// }
